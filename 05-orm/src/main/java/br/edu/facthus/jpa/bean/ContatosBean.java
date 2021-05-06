2a73991d60610bd7fceb78a4a25f2e029817d46e
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-05-06 20:21:44 -0300

    Acrescenta o projeto ORM

---
+++ 05-orm/src/main/java/br/edu/facthus/jpa/bean/ContatosBean.java
@@ -0,0 +1,32 @@
+package br.edu.facthus.jpa.bean;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.edu.facthus.jpa.entity.Contato;
+
+@Stateless
+public class ContatosBean {
+	
+	@SuppressWarnings("unused")
+	private static final Logger logger = 
+			Logger.getLogger(ContatosBean.class.getName());
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public void cadastra(Contato contato) {
+		entityManager.persist(contato);
+	}
+	
+	public List<Contato> lista() {
+		return entityManager
+				.createQuery("SELECT c FROM Contato c", Contato.class)
+				.getResultList();
+	}
+
+}
